2be1b723f0f325c98a23e397a8d5fefa9bc6890a
argrule <2039858744@qq.com>
2023-08-06 18:18:11 +0800

    :memo: feat: update readme

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,10 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+// import { AtButton } from 'taro-ui'
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
+// import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './index.scss'
+import Taro from '@tarojs/taro'
 
 export default class Index extends Component<PropsWithChildren<any>> {
   componentDidMount () { }
@@ -17,11 +18,14 @@ export default class Index extends Component<PropsWithChildren<any>> {
   render () {
     return (
       <View className='index'>
-        <Text>Hello!</Text>
-        <AtButton type='primary'>I need Taro UI</AtButton>
+        <Text>use text</Text>
+        <button onClick={sayhi} className='sayhi'>
+          Hello
+        </button>
+        {/* <AtButton type='primary'>I need Taro UI</AtButton>
         <Text>Taro UI 支持 Vue 了吗？</Text>
         <AtButton type='primary' circle={true}>支持</AtButton>
-        <AtButton type='secondary' circle={true} onClick={sayhi}>click</AtButton>
+        <AtButton type='secondary' circle={true} onClick={sayhi}>click</AtButton> */}
       </View>
     )
   }
@@ -29,4 +33,5 @@ export default class Index extends Component<PropsWithChildren<any>> {
 
 function sayhi() {
   console.log('hi')
+  Taro.navigateTo({url:'/pages/findCard/fc'})
 }
