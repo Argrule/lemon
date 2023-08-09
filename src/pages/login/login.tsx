c3cb5686807d7bc4226a328f6eb04718260326fc
argrule <2039858744@qq.com>
2023-08-09 10:38:13 +0800

    :bug: fix: add fix.d.ts

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,22 +1,37 @@
-import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
+import Taro,{useLoad} from '@tarojs/taro'
+import { AtButton } from 'taro-ui'
+import "taro-ui/dist/style/components/button.scss" // 按需引入
 import './login.scss'
 
+export default function Login() {
+    useLoad(() => {
+      console.log('login page loaded')
+    })
 
-export default class Login extends Component<PropsWithChildren> {
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
-  render () {
     return (
       <View className='login'>
-        <Text>Hello login!</Text>
+        <Text>sign in！</Text>
+        <AtButton type='primary' onClick={sayhi}>源神！ 启动！！</AtButton>
       </View>
     )
-  }
+}
+
+function sayhi() {
+  Taro.login({
+    success: function (res) {
+      if (res.code) {
+        console.log("get jscode =======:",res.code)
+        //发起网络请求
+        // Taro.request({
+        //   url: 'https://test.com/onLogin',
+        //   data: {
+        //     code: res.code
+        //   }
+        // })
+      } else {
+        console.log('登录失败！' + res.errMsg)
+      }
+    }
+  })
 }
