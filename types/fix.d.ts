bc5f9e16b4bab787c5d036a3d3a1b083b22f4903
argrule <2039858744@qq.com>
2023-08-09 17:52:44 +0800

    :arrow_up: chore: 更新taro-ui测试版

--- types/fix.d.ts
+++ types/fix.d.ts
@@ -1,6 +1,8 @@
 // 暂时解决taro-ui3 组件类型声明缺失children属性的问题
-declare module "react" {
-  interface Component {
-    children: React.ReactElement;
-  }
-}
+// declare module "react" {
+//   interface Component {
+//     children: React.ReactElement;
+//   }
+// }
+
+// 已经更新到taro-ui3.1.0-beta.4，修复了类型声明children缺失问题
