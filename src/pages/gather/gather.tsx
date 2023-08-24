175a191b14fd9757b913ab82ad95376665aeda0c
lizhengdong <3209562144@qq.com>
2023-08-24 15:35:19 +0800

    :sparkles: feat: 攒局页面基本元素

--- src/pages/gather/gather.tsx
+++ src/pages/gather/gather.tsx
@@ -1,13 +1,97 @@
-import { View } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import { View } from '@tarojs/components';
+import { AtButton, AtTag,AtFab,AtIcon } from 'taro-ui';
+import { useState } from 'react';
 
-import "taro-ui/dist/style/components/button.scss" // 按需引入
-import './gather.scss'
+import 'taro-ui/dist/style/components/button.scss';
+import 'taro-ui/dist/style/components/tag.scss';
+import "taro-ui/dist/style/components/fab.scss";
+import "taro-ui/dist/style/components/icon.scss";
+import './gather.scss';
 
-export default function Gather(){
-    return (
-      <View className='gather'>
-        <AtButton type='primary' circle={true} onClick={()=>console.log('hh')}>genshin impact launch!!!</AtButton>
+
+
+interface ClassificationItem {
+  name: string;
+  checked: boolean;
+}
+
+export default function Gather() {
+  const [classification, setClassification] = useState<ClassificationItem[]>([
+    { name: '空位', checked: false },
+    { name: '自习', checked: false },
+    { name: '电影', checked: false },
+    { name: '聚餐', checked: false },
+    { name: '拼车', checked: false },
+    { name: '拼单', checked: false },
+    { name: '运动', checked: false },
+    { name: '游戏', checked: false },
+    { name: '旅行', checked: false },
+    { name: '其他', checked: false },
+  ]);
+
+  const tagClick = (index: number) => {
+    if (classification[index].name === '空位') {
+      const updatedClassification = classification.map((item, i) => ({
+        ...item,
+        checked: i === index ? !item.checked : item.checked,
+      }));
+      setClassification(updatedClassification);
+    } else {
+      const updatedClassification = classification.map((item, i) => ({
+        ...item,
+        checked: item.name === '空位' ? item.checked : (i === index ? !item.checked : false),
+      }));
+      setClassification(updatedClassification);
+    }
+  };
+
+  const fixedButtonClick = () => {
+    console.log('fixedButtonClick');
+  };
+
+  return (
+    <View className='container'>
+      <View className='fixed-button'>
+        <AtFab onClick={() => fixedButtonClick()}>
+          <AtIcon value='search' size='25' color='white'></AtIcon>
+        </AtFab>
       </View>
-    )  
+
+      <View className='joinAndInitiate'>
+        <AtButton className='join-button' type='primary' circle>
+          我加入的局
+        </AtButton>
+        <AtButton className='initiate-button' type='primary' circle>
+          发起攒局
+        </AtButton>
+      </View>
+      <View className='tags'>
+        {classification.map((item, index) => (
+          <View
+            className={`tag ${item.checked ? 'checked' : ''}`}
+            key={index}
+            onClick={() => tagClick(index)}
+          >
+            <AtTag
+              className='tag-text'
+              type='primary'
+              circle
+              active={item.checked}
+            >
+              {item.name}
+            </AtTag>
+          </View>
+        ))}
+      </View>
+      <View className='cards'>
+        <View className='card'>
+          <View className='side'>
+            <View>拼</View>
+            <View>单</View>
+          </View>
+          <View className='content'></View>
+        </View>
+      </View>
+    </View>
+  );
 }
