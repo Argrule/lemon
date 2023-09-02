b1afdc3308b58069f745ad9b6adc291f5425370e
argrule <2039858744@qq.com>
2023-09-02 15:10:25 +0800

    :heavy_plus_sign: Feat: dayjs tool

---
+++ src/utils/dayjs.ts
@@ -0,0 +1,17 @@
+import dayjs from "dayjs";
+// import isLeapYear from "dayjs/plugin/isLeapYear"; // 导入插件
+import relativeTime from "dayjs/plugin/relativeTime"; // 导入插件
+import "dayjs/locale/zh-cn"; // 导入本地化语言
+
+// dayjs.extend(isLeapYear); // 使用插件
+dayjs.extend(relativeTime); // 使用插件
+dayjs.locale("zh-cn"); // 使用本地化语言
+
+/**
+ * @description 格式化时间转为距离当前时间的提示
+ * @param string time
+ * @returns 格式化后的时间提示
+ */
+export const FormatTimeFromNow = (time: string): string => {
+  return dayjs(time).fromNow();
+};
