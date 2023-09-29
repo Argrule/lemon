1d83460b8bb256dbb073952113011bcdbde02c5c
argrule <2039858744@qq.com>
2023-09-29 21:33:28 +0800

    :construction: Feat: hotpost layout

--- src/components/hotPost/hp.tsx
+++ src/components/hotPost/hp.tsx
@@ -3,11 +3,16 @@ import "./hp.scss";
 
 export const HotPost = () => {
   return (
-    <View className="flex hp-container" style={{ justifyContent: "space-evenly" }}>
-      <View>十大热帖</View>
+    <View
+      className="hp-container"
+      style={{ display: "flex", justifyContent: "space-evenly" }}
+    >
+      {/* 左侧文案 */}
+      <View className="flex-row hp-left">十大热帖</View>
+      {/* 右侧走马灯 */}
       <Swiper
-        className="test-h"
-        style={{ height: "60rpx", width: "80%" }}
+        className="hp-swiper"
+        style={{ height: "174rpx", width: "80%" }}
         indicatorColor="#999"
         indicatorActiveColor="#333"
         vertical
@@ -15,20 +20,13 @@ export const HotPost = () => {
         autoplay
         adjustVerticalHeight={"first"}
       >
-        <SwiperItem>
-          <View className="demo-text-1" style={{ background: "red" }}>
-            1
-          </View>
+        <SwiperItem style={{ borderRadius: "0 10px 10px 0" }}>
+          <View className="hp-swiper-item">1 测试的热搜帖子</View>
+          <View className="hp-swiper-item">2 测试的热搜帖子copy</View>
         </SwiperItem>
         <SwiperItem>
-          <View className="demo-text-2" style={{ background: "red" }}>
-            2
-          </View>
-        </SwiperItem>
-        <SwiperItem>
-          <View className="demo-text-3" style={{ background: "red" }}>
-            3
-          </View>
+          <View className="hp-swiper-item">3</View>
+          <View className="hp-swiper-item">4</View>
         </SwiperItem>
       </Swiper>
     </View>
