0c5fdec9b97dcb9dceb18a98f7be99ec12b1e4c1
argrule <2039858744@qq.com>
2023-11-02 10:53:58 +0800

    :art: fix: 审核七天

--- src/pages/forum/special.ts
+++ src/pages/forum/special.ts
@@ -2,7 +2,7 @@ import { Item } from "./data";
 
 // const delay = 5 * 60 * 1000;// 5分钟
 // const delay = 1 * 24 * 60 * 60 * 1000; // 1天
-// const delay = 2 * 24 * 60 * 60 * 1000; // 2天
+const delay = 7 * 24 * 60 * 60 * 1000; // 7天
 /**
  * @description 帖子特殊处理
  * @param post 帖子
@@ -15,8 +15,8 @@ export default function SpecialDeal(post: Item): boolean {
     return true;
   }
   // 为了过审核，所以前两天不显示
-  // if (new Date().getTime() < new Date(post.createTime).getTime() + delay) {
-  //   return true;
-  // }
+  if (new Date().getTime() < new Date(post.createTime).getTime() + delay) {
+    return true;
+  }
   return false;
 }
